20279c9615c1663790f78e8fbf20b4a01ab1c0a3
Renato Cavalcante <renatofh@hotmail.com>
2021-06-14 21:40:11 -0300

    API Spotify

--- API_Spotify/Spotify/Spotify.cs
+++ API_Spotify/Spotify/Spotify.cs
@@ -3,24 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Web;
-//using System.Web.Script.Serialization;
+
 using Newtonsoft.Json;
+using SpotifyAPI.Web;
 
 namespace API_Spotify.Spotify
 {
     public class Spotify
     {
-        public object getSpotifyListByType(int genre)
+        // Authorization: Bearer BQDGiNauhAKeyexB_ZhZMS4O4J9odX_0VTSmTMw1QEYtJ59ySvmO58z6fXqgz7rS9yTAQZmGYyX-EcVcnHEqa3u6xVBbUZvpKxvAT_qgRmuTLY_4W6P-qikGHayPR8vcJnJ6w-oi55qgIZTIEyDTLmg0xTWC5Ls
+        // https: //api.spotify.com/v1/albums
+        // https: //api.spotify.com/v1/recommendations/available-genre-seeds
+        // https: //open.spotify.com/album/3a0UOgDWw2pTajw85QPMiz?highlight=spotify:track:6rqhFgbbKwnb9MLmUQDhG6
+        // https: //api.spotify.com/v1/playlists/3cEYpjA9oz9GiPac4AsH4n?market=BR&fields=items(added_by.id%2Ctrack(name%2Chref%2Calbum(name%2Chref)))" -H 
+        // "Accept: application/json" -H 
+        // "Content-Type: application/json" -H 
+        // "Authorization: Bearer BQDGiNauhAKeyexB_ZhZMS4O4J9odX_0VTSmTMw1QEYtJ59ySvmO58z6fXqgz7rS9yTAQZmGYyX-EcVcnHEqa3u6xVBbUZvpKxvAT_qgRmuTLY_4W6P-qikGHayPR8vcJnJ6w-oi55qgIZTIEyDTLmg0xTWC5Ls"
+        //url: _baseUri + '/recommendations/available-genre-seeds'
+
+        public object getSpotifyListByGenre(int genre)
         {
-            var baseUri = "https://api.spotify.com/v1";
+            var spotify = new SpotifyClient("YourAccessToken");
+
+            var baseUri = $"https://api.spotify.com/v1";
             var accessToken = "d60d720e5428f4cf880e4d04aa424ae7";
             //var promiseImplementation = null;
 
             string url = $"" + baseUri + "/listbygenre?g=" + genre + "&token=" + accessToken;
             var client = new WebClient();
             var content = client.DownloadString(url);
-            //var serializer = new JavaScriptSerializer();
-            //var jsoncontent = serializer.Deserialize<Object>(content);
             object o = JsonConvert.DeserializeObject(content);
             string jsoncontent = JsonConvert.SerializeObject(o, Formatting.Indented);
             return jsoncontent;
